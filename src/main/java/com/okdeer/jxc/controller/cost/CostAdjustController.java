56b27128f2411ad7c600ecccd809119d2073fadd
yangyq02 <yangyq02@yschome.com>
2016-10-12 11:01:45 +0800

    添加成本调整路径

---
+++ src/main/java/com/okdeer/jxc/controller/cost/CostAdjustController.java
@@ -0,0 +1,18 @@
+package com.okdeer.jxc.controller.cost;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.okdeer.jxc.controller.BaseController;
+import com.okdeer.jxc.form.entity.PurchaseForm;
+
+@Controller
+@RequestMapping("cost/costAdjust")
+public class CostAdjustController extends BaseController<PurchaseForm>{
+	@RequestMapping(value = "view")
+	public String view(String type, Model model) {
+		model.addAttribute("type", type);
+		return "cost/costAdjustList";
+	}
+}
