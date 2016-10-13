4677d976cc80c969f262d887052caaae439806c4
wangxl01 <wangxl01@yschome.com>
2016-10-13 16:00:56 +0800

    日期代码提交错了

--- src/main/webapp/static/js/views/stockAdjust/stockList.js
+++ src/main/webapp/static/js/views/stockAdjust/stockList.js
@@ -6,7 +6,7 @@ $(function(){
 	//开始和结束时间
     $("#txtStartDate").val(dateUtil.getCurrDayPreOrNextDay("prev",30));
     $("#txtEndDate").val(dateUtil.getCurrentDate().format("yyyy-MM-dd"));
-    toChangeDate(0);
+    
     initDatagridRequireOrders();
 });
 var gridHandel = new GridClass();
