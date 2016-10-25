33532f3fe149bbff7eccd423c7203fc671656c15
liwb <liwb@yschome.com>
2016-10-25 15:43:47 +0800

    修复登录失败跳转空白页面的问题

--- src/main/java/com/okdeer/jxc/controller/system/SSOMainController.java
+++ src/main/java/com/okdeer/jxc/controller/system/SSOMainController.java
@@ -1,3 +1,4 @@
+
 package com.okdeer.jxc.controller.system;
 
 import java.io.IOException;
@@ -17,11 +18,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
-import com.okdeer.jxc.common.utils.EasyUiUtils;
-import com.okdeer.jxc.controller.BaseController;
-import com.okdeer.jxc.system.entity.SysUser;
-import com.okdeer.jxc.system.service.SysUserServiceApi;
-import com.okdeer.jxc.utils.UserUtil;
 import com.okdeer.base.common.utils.EncryptionUtils;
 import com.okdeer.ca.api.common.ApiException;
 import com.okdeer.ca.api.common.SysModuleMenuDto;
@@ -29,6 +25,11 @@ import com.okdeer.ca.api.common.SystemUserDto;
 import com.okdeer.ca.api.common.enums.SystemCodeEnum;
 import com.okdeer.ca.api.sysuser.entity.SysUserDto;
 import com.okdeer.ca.api.sysuser.service.ISysUserApi;
+import com.okdeer.jxc.common.utils.EasyUiUtils;
+import com.okdeer.jxc.controller.BaseController;
+import com.okdeer.jxc.system.entity.SysUser;
+import com.okdeer.jxc.system.service.SysUserServiceApi;
+import com.okdeer.jxc.utils.UserUtil;
 
 /**
  * ClassName: SSOMainController 
@@ -41,23 +42,23 @@ import com.okdeer.ca.api.sysuser.service.ISysUserApi;
  * ----------------+----------------+-------------------+-------------------------------------------
  *		进销存2.0	  2016年8月18日			 liwb			    系统权限Controller
  */
- 
+
 @Controller
 @RequestMapping(value = "/system")
-public class SSOMainController extends BaseController<SSOMainController>{
-	
-	
+public class SSOMainController extends BaseController<SSOMainController> {
+
 	@Value("${casServerUrlPrefix}")
 	private String casServerUrlPrefix;
-	
+
 	@Value("${serviceUrl}")
 	private String serviceUrl;
-	
+
 	@Reference(version = "1.0.0", check = false)
 	private SysUserServiceApi sysUserService;
-	
+
 	@Autowired
 	private ISysUserApi sysUserApi;
+
 	/**
 	 * 默认页面
 	 * 
@@ -80,7 +81,7 @@ public class SSOMainController extends BaseController<SSOMainController>{
 	 */
 	@RequestMapping(value = "logout")
 	public void logout(HttpServletResponse response) {
-		
+
 		try {
 			Subject subject = SecurityUtils.getSubject();
 			subject.logout();
@@ -90,7 +91,7 @@ public class SSOMainController extends BaseController<SSOMainController>{
 		}
 		return;
 	}
-	
+
 	/**
 	 * @Description: 带修改密码页面
 	 * @return
@@ -101,7 +102,7 @@ public class SSOMainController extends BaseController<SSOMainController>{
 	public String toUpdatePwd() {
 		return "system/updatePwd";
 	}
-	
+
 	/**
 	 * @Description: 修改密码
 	 * @param oldPass
@@ -117,32 +118,32 @@ public class SSOMainController extends BaseController<SSOMainController>{
 		SysUser user = UserUtil.getCurrentUser();
 		SystemUserDto sysUserDto = null;
 		try {
-			//获取当前用户的信息,拿到旧密码
+			// 获取当前用户的信息,拿到旧密码
 			sysUserDto = sysUserApi.loadSysUser(user.getId());
-			if(sysUserDto == null){
+			if (sysUserDto == null) {
 				return "获取用户中心数据失败,请稍后再试,或联系管理员";
 			}
 
 			String md5OldPass = EncryptionUtils.md5(oldPass);
 			String loginPassword = sysUserDto.getLoginPassword();
 
-			//校验旧密码
-			if(!loginPassword.equals(md5OldPass)){
+			// 校验旧密码
+			if (!loginPassword.equals(md5OldPass)) {
 				return "旧密码输入错误";
 			}
 			SysUserDto sysDto = new SysUserDto();
-			
+
 			sysDto.setId(sysUserDto.getId());
 			sysDto.setUpdateUserId(sysUserDto.getId());
-			//修改密码,传入明文,用户中心会进行md5加密
+			// 修改密码,传入明文,用户中心会进行md5加密
 			sysDto.setLoginPassword(newPass);
 			sysUserApi.edit(sysDto);
-			
-			//获取用户中心修改密码后的数据
+
+			// 获取用户中心修改密码后的数据
 			sysUserDto = sysUserApi.loadSysUser(user.getId());
 			loginPassword = sysUserDto.getLoginPassword();
-			
-			//更新本地用户密码
+
+			// 更新本地用户密码
 			sysUserService.updatePwd(sysUserDto.getId(), loginPassword);
 			return EasyUiUtils.SUCCESS;
 		} catch (ApiException e) {
@@ -153,25 +154,26 @@ public class SSOMainController extends BaseController<SSOMainController>{
 			return "修改密码失败";
 		}
 	}
-	
+
 	/**
 	 * 当前登录用户的权限集合
 	 */
 	@RequestMapping("permission/i/json")
 	@ResponseBody
 	public List<SysModuleMenuDto> myPermissionDate() {
-		List<SysModuleMenuDto>   sysModuleMenuDtosList = null;
+		List<SysModuleMenuDto> sysModuleMenuDtosList = null;
 		try {
-			sysModuleMenuDtosList =  sysUserApi.findMenuBySysUserId(UserUtil.getCurrentUser().getId(), SystemCodeEnum.JXC);
-			
+			sysModuleMenuDtosList = sysUserApi.findMenuBySysUserId(UserUtil.getCurrentUser().getId(),
+					SystemCodeEnum.JXC);
+
 			if (sysModuleMenuDtosList == null) {
 				sysModuleMenuDtosList = Lists.newArrayList();
 			}
 			LOG.info("当前用户权限:{}", JSON.toJSONString(sysModuleMenuDtosList));
 		} catch (ApiException e) {
-			LOG.error("获取权限菜单出错",e);
+			LOG.error("获取权限菜单出错", e);
 		}
 		return sysModuleMenuDtosList;
 	}
-	
+
 }
