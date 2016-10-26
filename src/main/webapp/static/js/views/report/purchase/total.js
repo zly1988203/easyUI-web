139f3626c6407c6857263f5e3fb30677c31d7cb0
李俊义 <lijy02@okdeer.com>
2016-10-26 18:11:22 +0800

    供应商汇总功能

--- src/main/webapp/static/js/views/report/purchase/total.js
+++ src/main/webapp/static/js/views/report/purchase/total.js
@@ -5,8 +5,38 @@ $(function() {
 	// 开始和结束时间
 	$("#txtStartDate").val(dateUtil.getPreMonthDate("prev",1).format("yyyy-MM-dd"));
 	$("#txtEndDate").val(dateUtil.getCurrentDate().format("yyyy-MM-dd"));
-	initPurReportTotalGrid();
+	initPurReportTotalGrid('data');
+	
+	//选择报表类型
+	changeType();
 });
+
+var flushFlg = false;
+function changeType(){
+	$(".radioItem").change(function(){
+		flushFlg = true;
+    	var a = $(this).val();
+    	if (a=="goodsTotal") {
+			// 初始化列表按收银员汇总
+    		initPurReportTotalGrid();
+		}else if (a=="supplierTotal") {
+			// 初始化列表按日期汇总
+			initPurReportSupplierGrid();
+		} else if (a=="formNoTotal") {
+			// 初始化列表按门店汇总
+			initCashDailymdGrid();
+		} else if (a=="categoryTotal") {
+			// 初始化列表按日期汇总
+			initCashDailydateGrid();
+			
+		}else if (a=="category") {
+			// 初始化列表按日期汇总
+			initCashDailydateGrid();
+			
+		}
+    	
+    });
+}
 var gridHandel = new GridClass();
 /**
  * 单据类型
@@ -19,7 +49,7 @@ function onChangeFormType(newV,oldV){
 
 var gridHandel = new GridClass();
 /**
- * 初始化表格
+ * 初始化表格按  商品
  * @param queryType
  */
 function initPurReportTotalGrid(queryType) {
@@ -111,7 +141,106 @@ function initPurReportTotalGrid(queryType) {
 			updateFooter();
 		}
     });
+    if(flushFlg){
+    	purchaseTotalCx();
+    }
 }
+/**
+ * 初始化表格按  供应商
+ * @param queryType
+ */
+function initPurReportSupplierGrid(queryType) {
+	gridHandel.setGridName("purReportTotal");
+    $("#purReportTotal").datagrid({
+        //title:'普通表单-用键盘操作',
+        method: 'post',
+        align: 'center',
+        //url: "",
+        //toolbar: '#tb',     //工具栏 id为tb
+        singleSelect: false,  //单选  false多选
+        rownumbers: true,    //序号
+        pagination: true,    //分页
+        //fitColumns:true,    //占满
+        showFooter:true,
+        pageSize : 20,
+        showFooter:true,
+        height:'100%',
+        columns: [[
+            {field: 'supplierCode', title: '供应商编号', width: 100, align: 'left',
+            	formatter : function(value, row,index) {
+                    var str = value;
+                    if(row.isFooter){
+                        str ='<div class="ub ub-pc ufw-b">合计</div> '
+                    }
+                    return str;
+                },
+            },
+            {field: 'supplierName', title: '供应商名称', width: 100, align: 'left'},
+            {field: 'realNumPI', title: '进货数量', width:120, align: 'right',
+            	formatter:function(value,row,index){
+                    if(row.isFooter){	
+                        return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                    }
+                    return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                }
+            },
+            {field: 'amountPI', title: '进货金额', width:120, align: 'right',
+            	formatter:function(value,row,index){
+                    if(row.isFooter){
+                        return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                    }
+                    return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                }
+            },
+            {field: 'realNumPR', title: '退货数量', width:120, align: 'right',
+            	formatter:function(value,row,index){
+                    if(row.isFooter){
+                        return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                    }
+                    return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                }
+            },
+            {field: 'amountPR', title: '退货金额', width:120, align: 'right',
+            	formatter:function(value,row,index){
+                    if(row.isFooter){
+                        return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                    }
+                    return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                }
+            },
+            {field: 'totalNum', title: '小计数量', width:120, align: 'right',
+            	formatter:function(value,row,index){
+                    if(row.isFooter){
+                        return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                    }
+                    return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                }
+            },
+            {field: 'totalAmount', title: '小计金额', width:120, align: 'right',
+            	formatter:function(value,row,index){
+                    if(row.isFooter){
+                        return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                    }
+                    return '<b>'+parseFloat(value||0).toFixed(2)+'</b>';
+                }
+            },
+            {field: 'validUserName', title: '审核人', width: 100, align: 'left'},
+            {field: 'validTime', title: '审核时间', width: 100, align: 'left',
+            	formatter : function(value, rowData, rowIndex) {
+            		return formatDate(value,'yyyy-MM-dd hh:mm');
+            	}
+            }
+        ]],
+		onLoadSuccess:function(data){
+			gridHandel.setDatagridHeader("center");
+			updateFooter();
+		}
+    });
+    if(flushFlg){
+    	purchaseTotalCx();
+    }
+}
+
 //合计
 function updateFooter(){
     var fields = {realNumPI:0,amountPI:0,realNumPR:0,amountPR:0,totalNum:0,totalAmount:0,};
