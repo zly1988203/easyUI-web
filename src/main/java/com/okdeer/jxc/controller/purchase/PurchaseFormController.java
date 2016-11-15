25764179bf1c085e382ea7a183a90a01b7889a7e
zhongy <zhongy@YSC-ZHONGY.kingser.com>
2016-11-15 14:41:42 +0800

    修改导出模板下载名称

--- src/main/java/com/okdeer/jxc/controller/purchase/PurchaseFormController.java
+++ src/main/java/com/okdeer/jxc/controller/purchase/PurchaseFormController.java
@@ -1055,16 +1055,16 @@ public class PurchaseFormController extends
 			String templateName = "";
 			if (Constant.ZERO == type) {
 				templateName = ExportExcelConstant.PURCHASE_GOODS_SKUCODE_TEMPLE;
-				fileName = "商品货号导入模板";
+				fileName = "货号导入模板";
 			} else if (Constant.ONE == type) {
 				templateName = ExportExcelConstant.PURCHASE_GOODS_BARCODE_TEMPLE;
-				fileName = "商品条码导入模板";
+				fileName = "条码导入模板";
 			} else if (Constant.TWO == type) {
 				templateName = ExportExcelConstant.GOODS_INTRODUCE_SKU_CODE_TEMPLE;
-				fileName = "商品引入货号导入模板";
+				fileName = "货号导入模板";
 			} else if (Constant.THREE == type) {
 				templateName = ExportExcelConstant.GOODS_INTRODUCE_BAR_CODE_TEMPLE;
-				fileName = "商品引入条码导入模板";
+				fileName = "条码导入模板";
 			}
 			if (StringUtils.isNotBlank(fileName)
 					&& StringUtils.isNotBlank(templateName)) {
