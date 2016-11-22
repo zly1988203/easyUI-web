a4d01cd8af2f11336ad18c9ca8c78dc3e1bdb583
zhongy <zhongy@YSC-ZHONGY.kingser.com>
2016-11-22 14:59:54 +0800

    商品查询优化

--- src/main/java/com/okdeer/jxc/controller/report/GoodsReportController.java
+++ src/main/java/com/okdeer/jxc/controller/report/GoodsReportController.java
@@ -125,7 +125,7 @@ public class GoodsReportController extends
 		LOG.info("商品查询导出execl：vo" + qo);
 		try {
 			// 如果没有选择店铺，则查询登录人所在机构的商品
-			if (StringUtils.isEmpty(qo.getBranchId())) {
+			if (StringUtils.isEmpty(qo.getBranchName())) {
 				qo.setBranchId(UserUtil.getCurrBranchId());
 			}
 			List<GoodsReportVo> exportList = goodsReportService.queryList(qo);
