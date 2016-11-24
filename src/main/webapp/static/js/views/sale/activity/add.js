d931e5999a5ff422da6770142d41f6cef903d270
liwb <liwb@yschome.com>
2016-11-24 11:02:19 +0800

    页面全角问题

--- src/main/webapp/static/js/views/sale/activity/add.js
+++ src/main/webapp/static/js/views/sale/activity/add.js
@@ -1165,7 +1165,7 @@ function saveActivity(){
   
   var check = {skuCode:'1'};
   
-  if(activityScopedis == 1 ||　activityScopemj == 1){
+  if(activityScopedis == 1 || activityScopemj == 1){
 	  var check ={goodsCategoryCode:'1'}
   }
   // 获取非空的数据
