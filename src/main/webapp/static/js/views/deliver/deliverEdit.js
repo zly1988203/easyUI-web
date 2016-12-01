0d6199f42e745da64c2c8708d2a65d59da1639e7
zhangchm <zhangchm@okdeer.com>
2016-12-01 16:50:26 +0800

    回车查询商品资料

--- src/main/webapp/static/js/views/deliver/deliverEdit.js
+++ src/main/webapp/static/js/views/deliver/deliverEdit.js
@@ -2,6 +2,7 @@
  * Created by zhanghuan on 2016/8/30.
  * 要货单-编辑
  */
+var branchId = '';
 $(function(){
     initDatagridEditRequireOrder();
     $("div").delegate("button","click",function(){
@@ -32,6 +33,7 @@ function initDatagridEditRequireOrder(){
                 gridHandel.addRow(parseInt(gridHandel.getSelectRowIndex())+1,gridDefault);
                
             }else{
+            	branchId = $("#sourceBranchId").val();
                 selectGoods(arg);
             }
         },
@@ -294,18 +296,19 @@ function selectGoods(searchKey){
         messager("请先选择发货机构");
         return;
     }
-    var targetBranchType = $("#targetBranchType").val();
+   /* var targetBranchType = $("#targetBranchType").val();
     // C类加盟店显示为发货机构的商品表
     if (targetBranchType === '5') {
     	targetBranchId = sourceBranchId;
-    }
+    }*/
     new publicGoodsService("DA",function(data){
         if(searchKey){
             $("#"+gridHandel.getGridName()).datagrid("deleteRow", gridHandel.getSelectRowIndex());
             $("#"+gridHandel.getGridName()).datagrid("acceptChanges");
         }
-        selectStockAndPrice(sourceBranchId,targetBranchId,data);
-    },searchKey,'',sourceBranchId,targetBranchId,targetBranchId,'');
+        selectStockAndPrice(data);
+    },searchKey,'',sourceBranchId,targetBranchId,branchId,'');
+    branchId = '';
 }
 
 //二次查询设置值
@@ -316,7 +319,11 @@ function setDataValue(data) {
         }
          var nowRows = gridHandel.getRowsWhere({skuCode:'1'});
          var addDefaultData = gridHandel.addDefault(data,gridDefault);
-         var keyNames = {
+         var keyNames = type==1?{
+	         id:'skuId',
+	         disabled:'',
+	         pricingType:''
+         }:{
     		 distributionPrice:'price',
 	         id:'skuId',
 	         disabled:'',
@@ -332,7 +339,8 @@ function setDataValue(data) {
 
 //查询价格、库存
 function selectStockAndPrice(sourceBranchId,targetBranchId,data){
-	var targetBranchType = $("#targetBranchType").val();
+	setDataValue(data);
+	/*var targetBranchType = $("#targetBranchType").val();
 	var type;
     // C类加盟店显示为发货机构的商品表
     if (targetBranchType === '5' || targetBranchType === '4') {
@@ -365,7 +373,7 @@ function selectStockAndPrice(sourceBranchId,targetBranchId,data){
     	error:function(result){
     		successTip("请求发送失败或服务器处理失败");
     	}
-    });
+    });*/
 }
 
 
