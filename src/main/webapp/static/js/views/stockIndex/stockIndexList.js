3a960bf26266e2c95897e4f1bb765e9b5bd3537d
xuyq <xuyq@yschome.com>
2017-02-16 15:25:40 +0800

    开发库存周转率，库存异常查询等功能页面

--- src/main/webapp/static/js/views/stockIndex/stockIndexList.js
+++ src/main/webapp/static/js/views/stockIndex/stockIndexList.js
@@ -21,37 +21,33 @@ var stockDetailList;
 function initDatagridStockIndex(){
 	gridHandel.setGridName('stockIndexList');
 	stockList = $("#stockIndexList").datagrid({
-		//title:'普通表单-用键盘操作',
 		method:'post',
 		align:'center',
-		//toolbar: '#tb',     //工具栏 id为tb
 		singleSelect:false,  //单选  false多选
 		rownumbers:true,    //序号
 		pagination:true,    //分页
 		fitColumns:true,    //每列占满
-		//fit:true,            //占满
 		height:'100%',
 		width:'100%',
 		columns:[[
 			{field:'check',checkbox:true},
 			{field: 'branchCode', title: '机构编号', width: 100, align: 'left'},
 			{field: 'branchName', title: '机构名称', width: 180, align: 'left'},
-			/*skuCode*/
-			{field: 'formNo', title: '货号', width: 100, align: 'left',formatter:function(value,row,index){
+			{field: 'categoryCode', title: '类别编号', width: 100, align: 'left'},
+			{field: 'categoryName', title: '类别名称', width: 120, align: 'left'},
+			{field: 'skuCode', title: '货号', width: 100, align: 'left',formatter:function(value,row,index){
 				return '<a style="text-decoration: underline;" href="#" onclick="openStockDialog(\''+value+'\',\''+index+'\')">'+value+'</a>'
 			}},
 			{field: 'skuName', title: '商品名称', width: 180, align: 'left'},
 			{field: 'barCode', title: '条码', width: 140, align: 'left'},
-			{field: 'spec', title: '规格', width: 80, align: 'left'},
-			{field: 'unit', title: '单位', width: 80, align: 'center'},
-			{field: 'categoryCode', title: '类别编号', width: 100, align: 'left'},
-			{field: 'category', title: '类别名称', width: 120, align: 'left'},
-			{field: 'stockBegin', title: '库存上限', width: 100, align: 'left'},
-			{field: 'stockEnd', title: '库存下限', width: 100, align: 'left'}
+			{field: 'skuSpec', title: '规格', width: 80, align: 'left'},
+			{field: 'skuUnit', title: '单位', width: 80, align: 'center'},
+			{field: 'lowerLimit', title: '库存下限', width: 100, align: 'left'},
+			{field: 'upperLimit', title: '库存上限', width: 100, align: 'left'}
 		]],
 
 	});
-    queryForm();
+//    queryForm();
 }
 
 function initDetailStock(){
@@ -63,36 +59,22 @@ function initDetailStock(){
 
 	//详情grid
 	stockDetailList = $("#detailStockTarget").datagrid({
-		//title:'普通表单-用键盘操作',
 		method:'post',
 		align:'center',
-		//toolbar: '#tb',     //工具栏 id为tb
 		singleSelect:false,  //单选  false多选
 		rownumbers:true,    //序号
 		fitColumns:true,    //每列占满
-		//fit:true,            //占满
 		height:'100%',
 		width:'100%',
 		columns:[[
-			{field: 'formNo', title: '货号', width: 100, align: 'left'},
-			{field: 'skuName', title: '商品名称', width: 180, align: 'left'},
+			{field: 'skuCode', title: '货号', width: 100, align: 'left'},
+			{field: 'skuName', title: '商品名称', width: 280, align: 'left'},
 			{field: 'barCode', title: '条码', width: 140, align: 'left'},
-			{field: 'spec', title: '规格', width: 80, align: 'left'},
-			{field: 'unit', title: '单位', width: 80, align: 'center'},
+			{field: 'skuSpec', title: '规格', width: 80, align: 'left'},
+			{field: 'skuUnit', title: '单位', width: 80, align: 'center'},
 			{field: 'categoryCode', title: '类别编号', width: 100, align: 'left'},
-			{field: 'category', title: '类别名称', width: 120, align: 'left'},
-			{field: 'stockBegin', title: '库存上限', width: 100, align: 'left',
-				editor:{
-					type:'numberbox',
-					value:0,
-					options:{
-						min:0,
-						precision:4,
-						onChange: onChangeStockBegin,
-					}
-				},
-			},
-			{field: 'stockEnd', title: '库存下限', width: 100, align: 'left',
+			{field: 'categoryName', title: '类别名称', width: 120, align: 'left'},
+			{field: 'lowerLimit', title: '库存下限', width: 100, align: 'left',
 				editor:{
 					type:'numberbox',
 					value:0,
@@ -100,6 +82,17 @@ function initDetailStock(){
 						min:0,
 						precision:4,
 						onChange: onChangeStockEnd,
+					}
+				},
+			},
+			{field: 'upperLimit', title: '库存上限', width: 100, align: 'left',
+				editor:{
+					type:'numberbox',
+					value:0,
+					options:{
+						min:0,
+						precision:4,
+						onChange: onChangeStockBegin,
 					}
 				},
 			}
@@ -118,14 +111,21 @@ function initDetailStock(){
 	});
 }
 
+
 function onChangeStockBegin(newV,oldV){
 	console.log('newV',newV);
 	console.log('oldV',oldV);
+	var gridVData = stockDetailList.datagrid('getData');
+	var currentRow = gridVData.rows;
+	currentRow[0].upperLimit = newV;
 }
 
 function onChangeStockEnd(newV,oldV){
 	console.log('newV',newV);
 	console.log('oldV',oldV);
+	var gridVData = stockDetailList.datagrid('getData');
+	var currentRow = gridVData.rows;
+	currentRow[0].lowerLimit = newV;
 }
 
 
@@ -140,6 +140,7 @@ function openStockDialog(value,index){
 	$("#detailBranchId").val(currentData.branchCode);
 	tempGridData.rows = [currentData];
 	tempGridData.list = [currentData];
+	console.log('tempGridData',tempGridData)
 	gridHandelDetail.setLoadData(tempGridData);
 	$('#detailDailog').dialog('open');
 
@@ -158,50 +159,46 @@ function saveDetailStock(){
 	var detBranchName = $('#detailBranchName').val();
 	console.log('detBranchID',detBranchID);
 	console.log('detBranchName',detBranchName);
-
-	var parentCurData = gridHandel.getRows()[detailIndex];
-	parentCurData = $.extend(parentCurData,detGridData);
-
-	console.log(parentCurData);
-	$("#stockIndexList").datagrid('updateRow',{
-		index: detailIndex,
-		row: {branchName: '新名称'}
+	var reqObj = JSON.stringify([detGridData]);
+	// 调用后台保存方法，成功提示
+	$.ajax({
+        type: "POST",
+        url: contextPath+"/stock/index/saveStockIndex",
+        data: {"data":reqObj},
+        dataType: "json",
+        success: function(data){
+			if (data.code == 0) {
+				$("#stockIndexList").datagrid('updateRow',{
+					index: detailIndex,
+					row: {lowerLimit: detGridData.lowerLimit,upperLimit:detGridData.upperLimit}
+				});
+				successTip('修改成功');
+				closeDetailDialog();
+			} else {
+				// 失败提示
+				$.messager.alert('提示', data.message);
+			}
+		}
 	});
 
 }
 
-//详情选择机构
-function selectDetailBranches(){
-	new publicAgencyService(function(data){
-		$("#detailBranchId").val(data.branchesId);
-		$("#detailBranchName").val(data.branchName);
-	},'BF','');
-}
-
-
-//新增入库单
-function addStockForm(){
-	toAddTab("新增库存调整单",contextPath + "/stock/adjust/add");
-}
-
-//查询入库单
+//查询
 function queryForm(){
+	if($("#branchName").val()==""){
+        messager("请选择机构");
+        return;
+    } 
 	var fromObjStr = $('#queryForm').serializeObject();
 	$("#stockIndexList").datagrid("options").method = "post";
-	$("#stockIndexList").datagrid('options').url = contextPath + '/stock/adjust/getStockFormList';
+	$("#stockIndexList").datagrid('options').url = contextPath + '/stock/index/getStockIndexList';
 	$("#stockIndexList").datagrid('load', fromObjStr);
 }
 
 
 //批量修改
 function updateMore(){
-	var getCheck = $("#stockIndexList").datagrid('getChecked');
-	if(!getCheck || getCheck.length < 1){
-		messager("请选择至少一行数据");
-		return;
-	}
-
-	toAddTab("存量指标修改",contextPath + "/stock/index/add");
+	toAddTab("商品存量指标修改",contextPath + "/stock/index/add");
 
 }
 
@@ -210,7 +207,7 @@ function updateMore(){
  */
 function selectBranches(){
 	new publicAgencyService(function(data){
-//		$("#createBranchId").val(data.branchesId);
+		$("#createBranchId").val(data.branchesId);
 		$("#branchName").val(data.branchName);
 	},'BF','');
 }
