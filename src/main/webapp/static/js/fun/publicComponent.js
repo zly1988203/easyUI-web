69f228d667f7d451ea5985945987d8810a2039e5
zhaoly <zhaoly@okdeer.com>
2017-02-27 14:15:54 +0800

    修改公共组件

--- src/main/webapp/static/js/fun/publicComponent.js
+++ src/main/webapp/static/js/fun/publicComponent.js
@@ -1791,11 +1791,11 @@ function publicActivity(callback,param){
 function publicConfirmDialog(callback,param){
 	var dalogTemp = $('<div/>').dialog({
         href: contextPath+"/goods/goodsSelect/goPublicComfirmDialog",
-        width: 300,
-        height: 155,
-        title: param.title,
+        width: 'undefined'==typeof(param.width)?300:param.width,
+        height: 'undefined'==typeof(param.height)?155:param.height,
+        title: 'undefined'==typeof(param.title)?'提示':param.title,
         closable: true,
-        resizable: true,
+        resizable: false,
         onClose: function () {
             $(dalogTemp).panel('destroy');
         },
