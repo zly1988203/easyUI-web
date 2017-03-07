9dc698319e45c344c0b317420044f5885962074e
xuyq <xuyq@okdeer.com>
2017-03-07 16:47:59 +0800

    删除无用import

--- src/main/java/com/okdeer/jxc/controller/report/RotaRateReportController.java
+++ src/main/java/com/okdeer/jxc/controller/report/RotaRateReportController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
