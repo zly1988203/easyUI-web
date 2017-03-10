8a25bf892455862d783f5c0cf64ee138d9e442d8
zhaoly <zhaoly@okdeer.com>
2017-03-10 16:45:05 +0800

    楼盘查询页面

--- src/main/webapp/static/js/views/stocktaking/miss/missList.js
+++ src/main/webapp/static/js/views/stocktaking/miss/missList.js
@@ -25,11 +25,100 @@ function initDgTakeStockMiss(){
 
 	});
 }
+
+//查询
 function queryForm(){
+	var fromObjStr = $('#queryForm').serializeObject();
+	// 去除编码
+    fromObjStr.branchName = fromObjStr.branchName.substring(fromObjStr.branchName.lastIndexOf(']')+1)
+
+	$("#missList").datagrid("options").method = "post";
+	$("#missList").datagrid('options').url = contextPath + '/stocktaking/diffSearch/getDiffSearchList';
+	$("#missList").datagrid('load', fromObjStr);
 }
+
 function gFunRefresh(){
+	$("#queryForm").form('clear');
 }
-function toClose(){
-}
+
+/**
+ * 导出
+ */
 function toExport(){
+	var length = $("#missList").datagrid('getData').total;
+	if(length == 0){
+		$.messager.alert('提示',"没有数据");
+		return;
+	}
+	var fromObjStr = $('#queryForm').serializeObject();
+	console.log(fromObjStr);
+	$("#queryForm").form({
+		success : function(data){
+			if(data==null){
+				$.messager.alert('提示',"导出数据成功！");
+			}else{
+				$.messager.alert('提示',JSON.parse(data).message);
+			}
+		}
+	});
+	$("#queryForm").attr("action",contextPath+"/stocktaking/diffSearch/exportDiffSearchList?"+fromObjStr);
+	$("#queryForm").submit();
+}
+
+//打印
+function toPrint(){
+	
+}
+
+//盘点批号
+function searchTakeStock(){
+	var branchId = $('#branchId').val();
+	var param = {
+			branchId:branchId
+	}
+	new publicStocktakingDialog(param,function(data){
+		console.log(data);
+		$("#branchId").val(data.branchId);
+		$("#branchName").val(data.branchName);
+		$("#batchId").val(data.id);
+		$("#batchNo").val(data.batchNo);
+	})
+}
+
+/**
+ * 机构名称
+ */
+function selectBranches(){
+	new publicAgencyService(function(data){
+		$("#branchId").val(data.branchesId);
+		$("#branchCode").val(data.branchCode);
+		$("#branchName").val(data.branchName);
+	},'BF','');
+}
+
+//选择商品
+function selectGoods(){
+	var branchId = $("#branchId").val();
+	var sourceBranchId = branchId;
+	var targetBranchId = branchId;
+    if(branchId == ""){
+        messager("请先选择机构");
+        return;
+    }
+    
+    var param = {
+    		type:'',
+    		key:"",
+    		isRadio:'1',
+    		branchId:branchId,
+    		sourceBranchId:'',
+    		targetBranchId:'',
+    		supplierId:'',
+    		flag:'0'
+    }
+    
+    new publicGoodsServiceTem(param,function(data){
+    	$('#skuId').val(data[0].skuId);
+    	$('#skuName').val(data[0].skuName);
+    });
 }
