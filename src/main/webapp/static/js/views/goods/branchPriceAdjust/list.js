60060e13cd90b68994afeca3c10852c929595c3b
liux01 <liux01@yschome.com>
2017-03-14 09:44:01 +0800

    门店调价单修改

--- src/main/webapp/static/js/views/goods/branchPriceAdjust/list.js
+++ src/main/webapp/static/js/views/goods/branchPriceAdjust/list.js
@@ -4,7 +4,7 @@
 var dg;
 $(function(){
     //开始和结束时间
-    $("#txtStartDate").val(dateUtil.getCurrDayPreOrNextDay("prev",30));
+	$("#txtStartDate").val(dateUtil.getPreMonthDate("prev",1).format("yyyy-MM-dd"));
     $("#txtEndDate").val(dateUtil.getCurrentDate().format("yyyy-MM-dd"));
     //单据状态切换
     changeStatus();
