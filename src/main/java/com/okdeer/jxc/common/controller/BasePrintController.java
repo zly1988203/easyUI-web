bccd7da861d9fd8b3e41ef5ae62f55a4d7abce8c
zhengwj <zhengwj@yschome.com>
2017-03-29 10:29:12 +0800

    提交长短款报表页面

--- src/main/java/com/okdeer/jxc/common/controller/BasePrintController.java
+++ src/main/java/com/okdeer/jxc/common/controller/BasePrintController.java
@@ -6,16 +6,23 @@
  */    
 package com.okdeer.jxc.common.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.okdeer.jxc.common.constant.ExportExcelConstant;
 import com.okdeer.jxc.common.utils.JsonMapper;
 import com.okdeer.jxc.controller.BaseController;
 import com.okdeer.jxc.utils.IOStreamUtils;
@@ -100,12 +107,72 @@ public abstract class BasePrintController<T,P> extends BaseController<T> {
 		
 		InputStream is = null;
 		try {
-			is = IOStreamUtils.getExcelExportPathInputStream(page+".xlsx");
+			// 出库单根据设置选择导出模板
+			if ("DOSheet".equalsIgnoreCase(page)) {
+				is = IOStreamUtils.getExcelExportPathInputStream(getExportFile(page));
+			} else {				
+				is = IOStreamUtils.getExcelExportPathInputStream(page+".xlsx");
+			}
 			//ReportExcelUtil.reportExcelToList(response, is, replaceMap.get("_订单编号").toString(), ReportExcelUtil.REPORT_XLSX, detailList);
 			ReportExcelUtil.reportExcelToMapAndList(response, is, replaceMap.get("_订单编号").toString(), ReportExcelUtil.REPORT_XLSX, replaceMap, detailList);
 		} catch (Exception e) {
 			LOG.error("导出失败：{}", e);
 		}
+	}
+	
+	/**
+	 * @Fields filePrefix : 七牛文件路径前缀
+	 */
+	@Value("${filePrefix}")
+	private String filePrefix;
+	
+	/**
+	 * @Description: 下载导出模板到本地
+	 * @param page 单据类型
+	 * @return 模板名称
+	 * @author zhengwj
+	 * @date 2017年3月28日
+	 */
+	private String getExportFile(String page) {
+		// TODO 查询当前用户使用的模板
+		String key = "";
+		String filePath = ExportExcelConstant.TEMPLATE_EXPORT_DIR + page + key + ".xlsx";
+		File file = new File(filePath);
+		// 判断模板是否存在，不存在的需要下载
+		if (!file.exists()) {
+			try {
+				URL url = new URL(filePrefix + key);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				// 设置超时间为3秒
+				conn.setConnectTimeout(3 * 1000);
+				// 防止屏蔽程序抓取而返回403错误
+				// conn.setRequestProperty("User-Agent", "Mozilla/4.0
+				// (compatible; MSIE 5.0; Windows NT; DigExt)");
 
+				// 得到输入流
+				InputStream inputStream = conn.getInputStream();
+				// 获取自己数组
+				byte[] buffer = new byte[1024];
+				int len = 0;
+				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				while ((len = inputStream.read(buffer)) != -1) {
+					bos.write(buffer, 0, len);
+				}
+				bos.close();
+				byte[] getData = bos.toByteArray();
+
+				FileOutputStream fos = new FileOutputStream(file);
+				fos.write(getData);
+				if (fos != null) {
+					fos.close();
+				}
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (Exception e) {
+				LOG.error("七牛下载导出模板错误:{}", e);
+			}
+		}
+		return page + key + ".xlsx";
 	}
 }
---
+++ src/main/java/com/okdeer/jxc/controller/purchase/DirectReceiptController.java
@@ -0,0 +1,54 @@
+/** 
+ *@Project: okdeer-jxc-web 
+ *@Author: zhengwj
+ *@Date: 2017年3月28日 
+ *@Copyright: ©2014-2020 www.okdeer.com Inc. All rights reserved. 
+ */
+
+package com.okdeer.jxc.controller.purchase;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.okdeer.jxc.common.controller.BasePrintController;
+
+/**
+ * ClassName: DirectReceiptController 
+ * @Description: 直送收货controller
+ * @author zhengwj
+ * @date 2017年3月28日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
+@Controller
+@RequestMapping("directReceipt")
+public class DirectReceiptController //extends BasePrintController<DirectReceiptController, DirectReceipt> 
+{
+
+/*	*//**
+	 * (non-Javadoc)
+	 * @see com.okdeer.jxc.common.controller.BasePrintController#getPrintReplace(java.lang.String)
+	 *//*
+	@Override
+	protected Map<String, Object> getPrintReplace(String formNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	*//**
+	 * (non-Javadoc)
+	 * @see com.okdeer.jxc.common.controller.BasePrintController#getPrintDetail(java.lang.String)
+	 *//*
+	@Override
+	protected List<DirectReceipt> getPrintDetail(String formNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+
+}
