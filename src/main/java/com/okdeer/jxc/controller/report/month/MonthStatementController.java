37a2faab6909ef65391b3641f46a81ead58fc9e6
xuyq <xuyq@okdeer.com>
2017-04-06 09:48:04 +0800

    开发月结功能

--- src/main/java/com/okdeer/jxc/controller/report/month/MonthStatementController.java
+++ src/main/java/com/okdeer/jxc/controller/report/month/MonthStatementController.java
@@ -1,9 +1,21 @@
 package com.okdeer.jxc.controller.report.month;
 
+import javax.validation.Valid;
+
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.okdeer.jxc.common.result.RespJson;
+import com.okdeer.jxc.controller.BaseController;
+import com.okdeer.jxc.controller.report.TimeSectionSaleReportController;
+import com.okdeer.jxc.report.service.MonthStatementService;
+import com.okdeer.jxc.report.vo.MonthlyReport;
+import com.okdeer.jxc.system.entity.SysUser;
 
 /**
  *<p></p>
@@ -17,13 +29,32 @@ import org.springframework.web.servlet.ModelAndView;
  * ----------------+----------------+-------------------+-------------------------------------------
  *
  */
- 
+
 @RestController
 @RequestMapping("report/monthStatement")
-public class MonthStatementController {
+public class MonthStatementController extends BaseController<TimeSectionSaleReportController> {
+
+	@Reference(version = "1.0.0", check = false)
+	private MonthStatementService monthStatementService;
 	
 	@RequestMapping(value = "list")
 	public ModelAndView monthStatement(Model model) {
 		return new ModelAndView("report/month/monthStatement");
+	}
+
+	@RequestMapping(value = "/executeMonthStatement", method = RequestMethod.POST)
+	public RespJson executeMonthStatement(@Valid MonthlyReport mr, BindingResult validate) {
+		SysUser user = getCurrentUser();
+		RespJson respJson = RespJson.success();
+		try {
+			mr.setCreateUserId(user.getId());
+			mr.setUpdateUserId(user.getId());
+			
+			respJson = monthStatementService.executeMonthStatement(mr);
+		} catch (Exception e) {
+			LOG.error("月结处理异常：", e);
+			respJson = RespJson.error("月结处理异常!");
+		}
+		return respJson;
 	}
 }
