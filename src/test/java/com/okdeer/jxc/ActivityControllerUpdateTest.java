8250636e6fcbb152eb912a348e3ed5bc7acfb8b2
songwj <songwj@okdeer.com>
2017-04-27 10:18:23 +0800

    新增单元测试覆盖率报表  执行clean install命令

--- src/test/java/com/okdeer/jxc/ActivityControllerUpdateTest.java
+++ src/test/java/com/okdeer/jxc/ActivityControllerUpdateTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,6 +32,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.alibaba.fastjson.JSON;
 import com.okdeer.jxc.common.constant.Constant;
+import com.okdeer.jxc.config.test.ResourceConfig;
+import com.okdeer.jxc.config.test.ShiroConfiguration;
 import com.okdeer.jxc.sale.activity.vo.ActivityDetailVo;
 import com.okdeer.jxc.sale.activity.vo.ActivityVo;
 import com.okdeer.jxc.sale.enums.ActivityType;
@@ -47,9 +50,10 @@ import com.okdeer.jxc.system.entity.SysUser;
  *
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！ 
+@SpringApplicationConfiguration(classes = Application.class) // 指定我们SpringBoot工程的Application启动类
+@WebAppConfiguration(value = "src/main/webapp") // 由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+@ContextConfiguration(classes={ ResourceConfig.class, ShiroConfiguration.class })
 public class ActivityControllerUpdateTest {
 	
 	@Autowired  
