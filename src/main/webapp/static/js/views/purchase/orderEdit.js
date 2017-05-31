08fcb190ae0774b87bfb13fbf45f9a7f7958d9f9
zhangchm <zhangchm@okdeer.com>
2017-05-31 09:57:59 +0800

    bug 19357 采购订单新增时，录入箱数后，数量可以正常显示，但一换行再将光标回到该行时数量列会被清零

--- src/main/webapp/static/js/views/purchase/orderEdit.js
+++ src/main/webapp/static/js/views/purchase/orderEdit.js
@@ -319,7 +319,7 @@ function onChangeLargeNum(newV,oldV){
     
     n++;
     var _temNewNum = gridHandel.getFieldData(gridHandel.getSelectRowIndex(),'tmpLargeNum');
-    var temp_new = newV;
+    var temp_new = _temNewNum;
     if(Math.abs(temp_new) > 0 && !oldV){
     	newV = _temNewNum;
     };
