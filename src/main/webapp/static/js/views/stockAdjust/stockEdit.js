af551c980fae5d112fcb39d14a5786b3755a58a3
zhaoly <zhaoly@okdeer.com>
2017-06-08 18:21:48 +0800

    修改bug

--- src/main/webapp/static/js/views/stockAdjust/stockEdit.js
+++ src/main/webapp/static/js/views/stockAdjust/stockEdit.js
@@ -216,7 +216,7 @@ function initDatagridEditRequireOrder(){
     });
 
     if(hasCostPrice==false){
-        priceGrantUtil.grantCostPrice(dataGridId,["price","amount"])
+        priceGrantUtil.grantCostPrice(gridName,["price","amount"])
     }
 
 }
