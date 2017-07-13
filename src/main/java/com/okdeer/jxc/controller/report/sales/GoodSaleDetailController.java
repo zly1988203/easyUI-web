14302371be28968d5638da7e3b1963e06425ca77
yangyq02 <yangyq02@yschome.com>
2017-07-13 13:47:10 +0800

    添加机构分组菜单

--- src/main/java/com/okdeer/jxc/controller/report/sales/GoodSaleDetailController.java
+++ src/main/java/com/okdeer/jxc/controller/report/sales/GoodSaleDetailController.java
@@ -7,14 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.okdeer.jxc.common.report.DataRecord;
 import com.okdeer.jxc.common.report.ReportService;
 import com.okdeer.jxc.controller.common.ReportController;
+import com.okdeer.jxc.report.sale.GoodSaleDetailServiceApi;
 
 @Controller
 @RequestMapping("report/sale/goodSaleDetailReport")
 public class GoodSaleDetailController extends ReportController {
-
+	@Reference(version = "1.0.0", check = false)
+	GoodSaleDetailServiceApi goodSaleDetailServiceApi;
 	@RequestMapping(value = "view")
 	public String view() {
 		return "report/sale/goodSaleDetailReport";
@@ -22,7 +25,7 @@ public class GoodSaleDetailController extends ReportController {
 
 	@Override
 	public ReportService getReportService() {
-		return null;
+		return goodSaleDetailServiceApi;
 	}
 
 	@Override
