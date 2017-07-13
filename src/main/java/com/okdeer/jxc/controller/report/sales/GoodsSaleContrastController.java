2313f048adb51744e1d89c2b6f811ffedadc8510
zuowm <zuowm@yschome.com>
2017-07-13 10:47:50 +0800

    时段销售对比分析和货类销售对比分析文件创建

---
+++ src/main/java/com/okdeer/jxc/controller/report/sales/GoodsSaleContrastController.java
@@ -0,0 +1,41 @@
+/** 
+ *@Project: okdeer-jxc-web 
+ *@Author: zuowm
+ *@Date: 2017年7月13日 
+ *@Copyright: ©2014-2020 www.okdeer.com Inc. All rights reserved. 
+ */
+package com.okdeer.jxc.controller.report.sales;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.okdeer.jxc.controller.BaseController;
+
+/**
+ * ClassName: GoodsSaleContrastController 
+ * @Description: 货类销售对比分析controller
+ * @author zuowm
+ * @date 2017年7月13日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
+@Controller
+@RequestMapping("/report/goodsSaleContrast")
+public class GoodsSaleContrastController extends BaseController<GoodsSaleContrastController> {
+
+	/**
+	 * @Description: 跳转页面
+	 * @param model
+	 * @return
+	 * @author zuowm
+	 * @date 2017年7月13日
+	 */
+	@RequestMapping(value = "/list")
+	public String list() {
+		LOG.info("GoodsSaleContrastController start....");
+		return "report/sales/goodsSaleContrast";
+	}
+}
