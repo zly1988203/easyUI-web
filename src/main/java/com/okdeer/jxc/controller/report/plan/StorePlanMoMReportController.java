6580febafdf008a3b8c16e16b529b7c96a12c5dc
liwb <liwb@yschome.com>
2017-07-14 18:44:23 +0800

    门店计划日跟踪报表Controller

--- src/main/java/com/okdeer/jxc/controller/report/plan/StorePlanMoMReportController.java
+++ src/main/java/com/okdeer/jxc/controller/report/plan/StorePlanMoMReportController.java
@@ -6,16 +6,10 @@
  */    
 package com.okdeer.jxc.controller.report.plan;  
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.okdeer.jxc.common.report.DataRecord;
-import com.okdeer.jxc.common.report.ReportService;
-import com.okdeer.jxc.controller.common.ReportController;
+import com.okdeer.jxc.controller.BaseController;
 
 
 /**
@@ -32,71 +26,12 @@ import com.okdeer.jxc.controller.common.ReportController;
 
 @Controller
 @RequestMapping("target/storePlan/report/mom")
-public class StorePlanMoMReportController extends ReportController {
+public class StorePlanMoMReportController extends BaseController<StorePlanMoMReportController> {
 	
 	@RequestMapping(value = "toManager")
 	public String toManager() {
 		return "report/plan/storePlanMoMReport";
 	}
 
-	/**
-	 * (non-Javadoc)
-	 * @see com.okdeer.jxc.controller.common.ReportController#getReportService()
-	 */
-	@Override
-	public ReportService getReportService() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * (non-Javadoc)
-	 * @see com.okdeer.jxc.controller.common.ReportController#getParam(javax.servlet.http.HttpServletRequest)
-	 */
-	@Override
-	public Map<String, Object> getParam(HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * (non-Javadoc)
-	 * @see com.okdeer.jxc.controller.common.ReportController#getFileName()
-	 */
-	@Override
-	public String getFileName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * (non-Javadoc)
-	 * @see com.okdeer.jxc.controller.common.ReportController#getHeaders()
-	 */
-	@Override
-	public String[] getHeaders() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * (non-Javadoc)
-	 * @see com.okdeer.jxc.controller.common.ReportController#getColumns()
-	 */
-	@Override
-	public String[] getColumns() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * (non-Javadoc)
-	 * @see com.okdeer.jxc.controller.common.ReportController#formatter(com.okdeer.jxc.common.report.DataRecord)
-	 */
-	@Override
-	public void formatter(DataRecord dataRecord) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
