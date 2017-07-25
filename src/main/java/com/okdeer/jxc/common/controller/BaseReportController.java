e8e34a506949380d42bf857751041820bec735a3
zhangqin <zhangq@yschome.com>
2017-07-25 09:53:12 +0800

    修改日结和月结报表

---
+++ src/main/java/com/okdeer/jxc/common/controller/BaseReportController.java
@@ -0,0 +1,182 @@
+package com.okdeer.jxc.common.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.okdeer.jxc.common.result.RespJson;
+import com.okdeer.jxc.controller.BaseController;
+import com.okdeer.jxc.utils.PriceGrantUtil;
+import com.okdeer.retail.common.entity.colsetting.GridColumn;
+import com.okdeer.retail.common.page.EasyUIPageInfo;
+import com.okdeer.retail.common.price.DataAccessParser;
+import com.okdeer.retail.common.util.GridExportPrintUtils;
+import com.okdeer.retail.common.util.JacksonUtil;
+import com.okdeer.retail.facade.report.facade.BaseReportFacade;
+import com.okdeer.retail.facade.report.qo.BaseReportQo;
+import com.okdeer.retail.facade.report.vo.DaySumReportVo;
+
+/**
+ * 
+ * ClassName: BaseReportController 
+ * @Description: 报表BaseController
+ * @author zhangq
+ * @date 2017年7月22日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
+public abstract class BaseReportController<Q extends BaseReportQo, V> extends BaseController<V> {
+
+	/**
+	 * 
+	 * @Description: 获取跳转路径
+	 * @return String  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	protected abstract String getViewName();
+
+	/**
+	 * 
+	 * @Description: 获取Model对象
+	 * @param model
+	 * @return Model  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	protected abstract Model getModel(Model model);
+
+	/**
+	 * 
+	 * @Description: 获取查询对象
+	 * @param qo
+	 * @return Q  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	protected abstract Q getQueryObject(Q qo);
+
+	/**
+	 * 
+	 * @Description: 获取显示对象的class
+	 * @param qo
+	 * @return Class<V>  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	protected abstract Class<V> getViewObjectClass();
+
+	/**
+	 * 
+	 * @Description: 获取facade对象
+	 * @return BaseReportFacade<Q,V>  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	protected abstract BaseReportFacade<Q, V> getReportFade();
+
+	/**
+	 * 
+	 * @Description: 跳转报表首页
+	 * @return ModelAndView  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	@RequestMapping(value = "/list")
+	public String index(Model model) {
+		// 获取model对象
+		model = getModel(model);
+
+		// 无权限访问的字段
+		Set<String> forbiddenSets = PriceGrantUtil.getNoPriceGrantSets();
+		DataAccessParser parser = new DataAccessParser(getViewObjectClass(), forbiddenSets);
+		forbiddenSets = parser.getAllForbiddenSets();
+
+		// 所有列
+		List<GridColumn> gridColumnList = GridExportPrintUtils.getAccessGridColumns(getViewObjectClass(),
+				forbiddenSets);
+		model.addAttribute("columns", JacksonUtil.toJson(gridColumnList));
+
+		return getViewName();
+	}
+
+	/**
+	 * 
+	 * @Description: 获取报表数据
+	 * @param qo
+	 * @param pageNumber
+	 * @param pageSize
+	 * @return EasyUIPageInfo<V>  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	@RequestMapping(value = "/getDayReportList", method = RequestMethod.POST)
+	@ResponseBody
+	public EasyUIPageInfo<V> getReportList(Q qo, @RequestParam(value = "page", defaultValue = PAGE_NO) int pageNumber,
+			@RequestParam(value = "rows", defaultValue = PAGE_SIZE) int pageSize) {
+		try {
+			// 分页参数
+			qo.setPageNum(pageNumber);
+			qo.setPageSize(pageSize);
+
+			// 查询数据
+			EasyUIPageInfo<V> page = getReportFade().queryListPage(qo);
+			
+			List<Map<String,Object>> rowsMapList = new ArrayList<Map<String,Object>>();
+			List<V> rows = page.getRows();
+			for (V v : rows) {
+			}
+			
+			cleanAccessData(page.getRows());
+			cleanAccessData(page.getFooter());
+			return page;
+		} catch (Exception e) {
+			LOG.error("获取报表数据异常:{}", e);
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @Description: 导出
+	 * @param response
+	 * @param qo
+	 * @return RespJson  
+	 * @author zhangq
+	 * @date 2017年7月22日
+	 */
+	@RequestMapping(value = "/exportList", method = RequestMethod.POST)
+	@ResponseBody
+	public RespJson exportList(HttpServletResponse response, Q qo) {
+		RespJson resp = RespJson.success();
+		try {
+			// 查询结果
+			List<V> exportList = getReportFade().queryList(qo);
+
+			// 无权限访问的字段
+			Set<String> forbiddenSets = PriceGrantUtil.getNoPriceGrantSets();
+			DataAccessParser parser = new DataAccessParser(DaySumReportVo.class, forbiddenSets);
+			forbiddenSets = parser.getAllForbiddenSets();
+
+			// 导出
+			GridExportPrintUtils.exportAccessExcel(DaySumReportVo.class, exportList, forbiddenSets, response);
+		} catch (Exception e) {
+			LOG.error("导出日销售列表信息异常:{}", e);
+			resp = RespJson.error("导出日进销存报表异常");
+		}
+		return resp;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/report/month/monthSumGenerator.jsp
@@ -0,0 +1,82 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>日结</title>
+	<%@ include file="/WEB-INF/views/include/header.jsp"%>
+</head>
+<body>
+<body class="ub uw uh ufs-14 uc-black">
+	<div class="ub ub-ver ub-f1 umar-4 upad-4 ubor uh-200 ubgc-while">
+		<div class="ub umar-t10">
+			<div class="ub ub-ac">
+				<div class="umar-r10 uw-70 ut-r">机&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;构:</div>
+				<input type="hidden" id="branchId" name="branchId" /> <input
+					class="uinp ub ub-f1" type="text" id="branchName" name="branchName"
+					maxlength="50" readonly="readonly" onclick="selectBranches()" />
+				<div class="uinp-more" onclick="selectBranches()">...</div>
+			</div>
+			<div class="ub ub-ac umar-l20">
+				<div class="umar-r10 uw-70 ut-r">月结期间:</div>
+				<input class="Wdate" readonly="readonly" name="rptDate" id="rptDate"
+					onfocus="updateWdatePicker(0)" />
+			</div>
+		</div>
+		<div class="ub ub-ac umar-t10 upad-l10">月结周期： 月结日是每月1日，月结期间由此决定。
+		</div>
+		<div class="ub ub-ac umar-t10 upad-l10">
+			注：上一次月结期间为：
+			<div class="utxt" id="maxRptDate">${mReport.maxRptDate}</div>
+		</div>
+		<div class="ub uw-600 ub-ac ub-pc umar-t20">
+			<div class="ubtns ">
+				<div class="ubtns-item" onClick="monthStatement()">月结</div>
+			</div>
+		</div>
+	</div>
+	
+	<script type="text/javascript">
+		$(function() {
+			$("#branchName").val(sessionBranchName);
+			$("#branchId").val(sessionBranchId);
+			// 获得上月月结期间
+			getUpMonthReportDay(sessionBranchId);
+		});
+		
+		// 上一次月结期间
+		function getUpMonthReportDay(branchId) {
+			$.ajax({
+				url : contextPath + "/report/monthStatement/getUpMonthReportDay",
+				type : "POST",
+				data : {
+					"branchId" : branchId
+				},
+				success : function(result) {
+					var maxRptDate = result['data'].maxRptDate;
+					if (!maxRptDate) {
+						maxRptDate = "该机构尚无月结数据";
+					}
+					$("#maxRptDate").html(maxRptDate);
+				},
+				error : function(result) {
+					$_jxc.alert("请求发送失败或服务器处理失败");
+				}
+			});
+		}
+		
+		// 选择月结期间
+		function updateWdatePicker() {
+			WdatePicker({
+				dateFmt : 'yyyy-MM',
+				maxDate : '%y-%M',
+				onpicked : function(dp) {
+					$("input:radio[name='dateradio']").attr("checked", false);
+				}
+			});
+		}
+	</script>
+</body>
+</html>
