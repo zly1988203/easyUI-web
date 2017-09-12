a743982bf5c2d5739226edfb7327d5ce7e174756
zhaoly <zhaoly@okdeer.com>
2017-09-12 13:54:27 +0800

    表头拖动公共方法添加

--- src/main/webapp/static/js/fun/baseEasyui.js
+++ src/main/webapp/static/js/fun/baseEasyui.js
@@ -1716,130 +1716,90 @@ $.extend($.fn.validatebox.defaults.rules, {
 /*----------------jxc component js end  ---------------------------*/
 
 //表头拖动
-$.extend($.fn.datagrid.methods,{
-    columnMoving:function(jq){
-        return jq.each(function(){
-            var grid = this;
-
-            var directionDiv = $("<div></div>");
-
-            directionDiv.hide();
-
-            $("body").append(directionDiv);
-
-            $(grid).datagrid("getPanel")
-                .find(".datagrid-header td[field]:not(td[field='ckb'])").draggable({
-                revert:true,
-                cursor:"move",
-                deltaX:10,
-                deltaY:10,
-                edge:10,
-                proxy:function(source){
-                    var proxyEl = $("<div></div>");
-
-                    proxyEl.addClass("dg-proxy dg-proxy-error");
-
-                    proxyEl.text($(source).text());
-
-                    proxyEl.appendTo($("body"));
-
-                    return proxyEl;
+$.extend($.fn.datagrid.methods, {
+    columnMoving: function (jq) {
+        return jq.each(function () {
+            var target = this;
+            var cells = $(this).datagrid('getPanel').find('div.datagrid-header td[field]');
+            cells.draggable({
+                revert: true,
+                cursor: 'pointer',
+                edge: 5,
+                proxy: function (source) {
+                    var p = $('<div class="tree-node-proxy tree-dnd-no" style="position:absolute;border:1px solid #ff0000"/>').appendTo('body');
+                    p.html($(source).text());
+                    p.hide();
+                    return p;
+                },
+                onBeforeDrag: function (e) {
+                    e.data.startLeft = $(this).offset().left;
+                    e.data.startTop = $(this).offset().top;
+                },
+                onStartDrag: function () {
+                    $(this).draggable('proxy').css({
+                        left: -10000,
+                        top: -10000
+                    });
+                },
+                onDrag: function (e) {
+                    $(this).draggable('proxy').show().css({
+                        left: e.pageX + 15,
+                        top: e.pageY + 15
+                    });
+                    return false;
                 }
             }).droppable({
-                accept:".datagrid-header td[field]",
-                onDragOver:function(e,source){
-                    $(source).draggable("proxy").removeClass("dg-proxy-error").addClass("dg-proxy-right");
-
-                    $(".dg-hide-div").hide();
-
-                    var thisIndex = $(this).index();
-                    var sourceIndex = $(source).index();
-
-                    var className = null;
-
-                    var height = null;
-
-                    var thisOffset = null;
-
-                    var left = null;
-                    var top = null;
-
-                    height = $(this).height();
-
-                    if(sourceIndex > thisIndex){
-                        className = "dg-move-prev";
-
-                        thisOffset = $(this).offset();
-
-                        left = thisOffset.left;
-                        top = thisOffset.top;
-                    }else{
-                        className = "dg-move-next";
-
-                        if(thisIndex == $(this).parent().children(":last").index()){
-                            thisOffset = $(this).offset();
-
-                            left = thisOffset.left + $(this).width() - directionDiv.width();
-                            top = thisOffset.top;
-                        }else{
-                            thisOffset = $(this).next().offset();
-
-                            left = thisOffset.left - directionDiv.width();
-                            top = thisOffset.top;
-                        }
-                    }
-
-                    directionDiv.removeClass().addClass(className);
-                    directionDiv.css({height:height, left:left, top:top});
-                    directionDiv.show();
+                accept: 'td[field]',
+                onDragOver: function (e, source) {
+                    $(source).draggable('proxy').removeClass('tree-dnd-no').addClass('tree-dnd-yes');
+                    $(this).css('border-left', '1px solid #ff0000');
                 },
-                onDragLeave:function(e,source){
-                    $(source).draggable("proxy").removeClass("dg-proxy-right").addClass("dg-proxy-error");
-
-                    directionDiv.hide();
+                onDragLeave: function (e, source) {
+                    $(source).draggable('proxy').removeClass('tree-dnd-yes').addClass('tree-dnd-no');
+                    $(this).css('border-left', 0);
                 },
-                onDrop:function(e,source){
-                    directionDiv.remove();
-
-                    var thisIndex = $(this).index();
-                    var sourceIndex = $(source).index();
-
-                    var sourceCol = new Array();
-
-                    $(source).remove();
-                    $.each($(grid).datagrid("getPanel")
-                        .find(".datagrid-body tr"),function(index,obj){
-                        var sourceTd = $(obj).children("td:eq(" + sourceIndex + ")");
-
-                        sourceCol.push(sourceTd);
-
-                        sourceTd.remove();
-                    });
-
-                    var prev = sourceIndex > thisIndex;
-
-                    thisIndex = $(this).index();
-
-                    if(prev){
-                        $(this).before($(source));
-                    }else{
-                        $(this).after($(source));
-                    }
-
-                    $.each($(grid).datagrid("getPanel")
-                        .find(".datagrid-body tr"),function(index,obj){
-                        var thisTd = $(obj).children("td:eq(" + thisIndex + ")");
-
-                        if(prev){
-                            thisTd.before(sourceCol[index]);
-                        }else{
-                            thisTd.after(sourceCol[index]);
-                        }
-                    });
-
-                    $(grid).datagrid("columnMoving").datagrid("columnHiding");
+                onDrop: function (e, source) {
+                    $(this).css('border-left', 0);
+                    var fromField = $(source).attr('field');
+                    var toField = $(this).attr('field');
+                    setTimeout(function () {
+                        moveField(fromField, toField);
+                        $(target).datagrid();
+                        $(target).datagrid('columnMoving');
+                    }, 0);
                 }
             });
+
+            // move field to another location
+            function moveField(from, to) {
+                var columns = $(target).datagrid('options').columns;
+                var cc = columns[0];
+                var c = _remove(from);
+                if (c) {
+                    _insert(to, c);
+                }
+
+                function _remove(field) {
+                    for (var i = 0; i < cc.length; i++) {
+                        if (cc[i].field == field) {
+                            var c = cc[i];
+                            cc.splice(i, 1);
+                            return c;
+                        }
+                    }
+                    return null;
+                }
+                function _insert(field, c) {
+                    var newcc = [];
+                    for (var i = 0; i < cc.length; i++) {
+                        if (cc[i].field == field) {
+                            newcc.push(c);
+                        }
+                        newcc.push(cc[i]);
+                    }
+                    columns[0] = newcc;
+                }
+            }
         });
     }
 });
