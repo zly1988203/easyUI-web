f748a90b539eebbae4bfc16ecf67338a5fc722f3
zhaoly <zhaoly@okdeer.com>
2017-10-10 16:06:59 +0800

    优化代码

--- src/main/webapp/static/js/views/deliver/DoEdit.js
+++ src/main/webapp/static/js/views/deliver/DoEdit.js
@@ -516,7 +516,7 @@ function onSelectIsGift(data){
         var targetPrice = gridHandel.getFieldTarget('price');
         if(data.id=="1"){
             $(targetPrice).numberbox('setValue',0);
-            gridHandel.setFieldsData({price:oldPrice});//单价
+            gridHandel.setFieldsData({price:0});//单价
             gridHandel.setFieldValue('amount',0);//总金额
             gridHandel.setFieldValue('taxAmount',0);//税额
         }else{
