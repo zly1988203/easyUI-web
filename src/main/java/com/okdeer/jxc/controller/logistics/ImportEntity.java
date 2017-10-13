aa15b83a2c62e844bb44b30e73d562f5778e04f3
zhangchm <zhangchm@okdeer.com>
2017-10-13 10:28:18 +0800

    配送点退货导入修改

--- src/main/java/com/okdeer/jxc/controller/logistics/ImportEntity.java
+++ src/main/java/com/okdeer/jxc/controller/logistics/ImportEntity.java
@@ -4,47 +4,46 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 public class ImportEntity implements Serializable {
-
 	/**
 	 * @Fields serialVersionUID : serialVersionUID
 	 */
 	private static final long serialVersionUID = -421241876104597128L;
-
 	/**
 	 * 业务系统单号
 	 */
 	private String formNo;
-
 	/**
 	 * 物流系统单号
 	 */
 	private String logisticFormNo;
-
 	/**
 	 * 行号
 	 */
 	private Integer rowNo;
-
 	/**
 	 * 货号
 	 */
 	private String skuCode;
-
 	/**
 	 * 条码
 	 */
 	private String barCode;
-
 	/**
 	 * 数量
 	 */
 	private BigDecimal num;
-
 	/**
 	 * 赠品数量
 	 */
 	private BigDecimal giftNum;
-
+	/**
+	 * 不良品数量
+	 */
+	private BigDecimal rejectsNum;
+	/**
+	 * 搁置品数量
+	 */
+	private BigDecimal shelveNum;
 	/**
 	 * 备注
 	 */
@@ -114,11 +113,42 @@ public class ImportEntity implements Serializable {
 		this.remark = remark;
 	}
 
+	/**
+	 * @return the rejectsNum
+	 */
+	public BigDecimal getRejectsNum() {
+		return rejectsNum;
+	}
+
+	/**
+	 * @param rejectsNum the rejectsNum to set
+	 */
+	public void setRejectsNum(BigDecimal rejectsNum) {
+		this.rejectsNum = rejectsNum;
+	}
+
+	/**
+	 * @return the shelveNum
+	 */
+	public BigDecimal getShelveNum() {
+		return shelveNum;
+	}
+
+	/**
+	 * @param shelveNum the shelveNum to set
+	 */
+	public void setShelveNum(BigDecimal shelveNum) {
+		this.shelveNum = shelveNum;
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "ImportEntity [formNo=" + formNo + ", logisticFormNo=" + logisticFormNo + ", rowNo=" + rowNo
 				+ ", skuCode=" + skuCode + ", barCode=" + barCode + ", num=" + num + ", giftNum=" + giftNum
-				+ ", remark=" + remark + "]";
+				+ ", rejectsNum=" + rejectsNum + ", shelveNum=" + shelveNum + ", remark=" + remark + "]";
 	}
-
 }
