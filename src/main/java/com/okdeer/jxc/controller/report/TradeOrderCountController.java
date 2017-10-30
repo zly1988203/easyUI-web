66f176f9f3d83b1af854f80658c324fac71d6970
yangyq02 <yangyq02@yschome.com>
2017-10-30 13:29:03 +0800

    修改店铺销售排名导出

--- src/main/java/com/okdeer/jxc/controller/report/TradeOrderCountController.java
+++ src/main/java/com/okdeer/jxc/controller/report/TradeOrderCountController.java
@@ -7,6 +7,7 @@
 
 package com.okdeer.jxc.controller.report;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,6 +18,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
 import com.alibaba.dubbo.rpc.RpcContext;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -149,6 +151,7 @@ public class TradeOrderCountController extends BasePrintController<TradeOrderCou
 				vo.setBranchId(getCurrBranchId());
 			}
 			List<TradeOrderCountVo> exportList = tradeOrderCountServiceApi.queryTradeOrderCount(vo);
+			exportList.add(total(exportList,vo));
 			//刪除合計功能,建議採用excel的模板合計,提高效率
 			//TradeOrderCountVo tradeOrderCountVo = tradeOrderCountServiceApi.queryTradeOrderCountSum(vo);
 			//tradeOrderCountVo.setBranchName("合计：");
@@ -183,6 +186,51 @@ public class TradeOrderCountController extends BasePrintController<TradeOrderCou
 	@Override
 	protected BranchSpecServiceApi getBranchSpecService() {
 		return null;
+	}
+	//计算合计
+	private TradeOrderCountVo total(List<TradeOrderCountVo> list,TradeOrderCountQo qo){
+		TradeOrderCountVo total=new TradeOrderCountVo();
+		total.setBranchName("合计");
+		total.setCostAmount(BigDecimal.ZERO);
+		total.setDayAvgNum(BigDecimal.ZERO);
+		total.setGrossProfit(BigDecimal.ZERO);
+//		total.setGrossProfitRate(BigDecimal.ZERO);
+		total.setLinePrice(BigDecimal.ZERO);
+		total.setOffLinePrice(BigDecimal.ZERO);
+		total.setPrice(BigDecimal.ZERO);
+		total.setTotalAmount(BigDecimal.ZERO);
+		total.setTotalLineAmount(BigDecimal.ZERO);
+		total.setTotalLineNum(BigDecimal.ZERO);
+		total.setTotalLineSaleNum(BigDecimal.ZERO);
+		total.setTotalNum(BigDecimal.ZERO);
+		total.setTotalOffLineNum(BigDecimal.ZERO);
+		total.setTotalSaleNum(BigDecimal.ZERO);
+		for(TradeOrderCountVo vo:list){
+			total.setCostAmount(total.getCostAmount().add(vo.getCostAmount()));
+			total.setGrossProfit(total.getGrossProfit().add(vo.getGrossProfit()));
+			total.setTotalAmount(total.getTotalAmount().add(vo.getTotalAmount()));
+			total.setTotalLineAmount(total.getTotalLineAmount().add(vo.getTotalLineAmount()));
+			total.setTotalLineNum(total.getTotalLineNum().add(vo.getTotalLineNum()));
+			total.setTotalLineSaleNum(total.getTotalLineSaleNum().add(vo.getTotalLineSaleNum()));
+			total.setTotalNum(total.getTotalNum().add(vo.getTotalNum()));
+			total.setTotalOffLineNum(total.getTotalOffLineNum().add(vo.getTotalOffLineNum()));
+			total.setTotalSaleNum(total.getTotalSaleNum().add(vo.getTotalSaleNum()));
+		}
+		total.setGrossProfitRate(total.getGrossProfit().divide(total.getTotalAmount(),BigDecimal.ROUND_HALF_DOWN,4 ).multiply(BigDecimal.valueOf(100))+"%");
+		total.setPrice(isZero(total.getTotalNum())? total.getTotalAmount(): total.getTotalAmount().divide(total.getTotalNum(),BigDecimal.ROUND_HALF_DOWN,4));
+		total.setLinePrice(isZero(total.getTotalLineNum())?total.getTotalLineAmount(): total.getTotalLineAmount().divide(total.getTotalLineNum(),BigDecimal.ROUND_HALF_DOWN,4));
+		//线下客单价 (总金额-线上金额)/(总订单数-线上订单数)
+		total.setOffLinePrice(isZero(total.getTotalNum ().subtract(total.getTotalLineNum()))?total.getTotalAmount().subtract(total.getTotalLineAmount()): total.getTotalAmount().subtract(total.getTotalLineAmount()).divide(total.getTotalNum ().subtract(total.getTotalLineNum()),BigDecimal.ROUND_HALF_DOWN,4));
+		//计算日均销量
+		int days= DateUtils.pastDays(qo.getStartTime(),qo.getEndTime());
+		total.setDayAvgNum(total.getTotalNum().divide(BigDecimal.valueOf(days),BigDecimal.ROUND_HALF_DOWN,4));
+		return total;
+	}
+	private boolean isZero(BigDecimal val){
+		if(val==null||BigDecimal.ZERO.compareTo(val)==0){
+			return true;
+		}
+		return false;
 	}
 
 }
