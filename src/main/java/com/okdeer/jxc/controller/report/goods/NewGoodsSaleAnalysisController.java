b4c94b0992958984be5cb8029270622af97e91e2
Song Jason <swj0881@gmail.com>
2017-11-07 16:28:11 +0800

    新品销售分析查询异步优化

--- src/main/java/com/okdeer/jxc/controller/report/goods/NewGoodsSaleAnalysisController.java
+++ src/main/java/com/okdeer/jxc/controller/report/goods/NewGoodsSaleAnalysisController.java
@@ -7,26 +7,31 @@
 
 package com.okdeer.jxc.controller.report.goods;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.google.common.collect.Lists;
 import com.okdeer.jxc.common.constant.ExportExcelConstant;
 import com.okdeer.jxc.common.report.ReportService;
+import com.okdeer.jxc.common.utils.PageUtils;
 import com.okdeer.jxc.common.utils.StringUtils;
 import com.okdeer.jxc.controller.common.ReportController;
 import com.okdeer.jxc.report.goods.service.NewGoodsSaleAnalysisService;
 import com.okdeer.jxc.utils.UserUtil;
 import com.okdeer.retail.common.price.PriceConstant;
 import com.okdeer.retail.common.report.DataRecord;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * ClassName: NewGoodsSaleAnalysisController 
@@ -44,13 +49,42 @@ import com.okdeer.retail.common.report.DataRecord;
 @RequestMapping("report/newGoodsSaleAnalysis")
 public class NewGoodsSaleAnalysisController extends ReportController {
 
-	@Reference(version = "1.0.0", check = false)
-	private NewGoodsSaleAnalysisService newGoodsSaleAnalysisService;
+    //@Reference(version = "1.0.0", check = false)
+    @Resource
+    private NewGoodsSaleAnalysisService newGoodsSaleAnalysisService;
 
 	@RequestMapping("/toManager")
 	public String toManager(Model model) {
 		return "report/goods/newGoodsSaleAnalysis";
 	}
+
+    @Override
+    @RequestMapping("reportListPage")
+    @ResponseBody
+    public PageUtils<DataRecord> reportListPage(HttpServletRequest request,
+                                                @RequestParam(value = "page", defaultValue = PAGE_NO) Integer page,
+                                                @RequestParam(value = "rows", defaultValue = PAGE_SIZE) Integer rows) throws ExecutionException, InterruptedException, ExecutionException {
+        Map<String, Object> param = getParam(request);
+        newGoodsSaleAnalysisService.getListPage(param, page, rows);
+        Future<PageUtils<DataRecord>> listFuture = RpcContext.getContext().getFuture();
+        newGoodsSaleAnalysisService.getTotal(param);
+        Future<DataRecord> footerDataFuture = RpcContext.getContext().getFuture();
+        PageUtils<DataRecord> list = listFuture.get();
+        DataRecord footerData = footerDataFuture.get();
+        if (footerData == null) {
+            footerData = new DataRecord();
+        }
+        List<DataRecord> footer = Lists.newArrayList();
+        footer.add(footerData);
+        list.setFooter(footer);
+
+        for (DataRecord dataRecord : list.getList()) {
+            formatter(dataRecord);
+        }
+        cleanDataMaps(getPriceAccess(), list.getList());
+        cleanDataMaps(getPriceAccess(), list.getFooter());
+        return list;
+    }
 
 	/**
 	 * (non-Javadoc)
