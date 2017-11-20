0f01b84a8b428794fd5c4e9a094a8c8b7bd19288
Song Jason <swj0881@gmail.com>
2017-11-20 10:50:33 +0800

    修改直送配送缺貨率分析報表bug

--- src/main/webapp/static/js/views/report/direct/outOfStock.js
+++ src/main/webapp/static/js/views/report/direct/outOfStock.js
@@ -425,6 +425,9 @@ function getColumnsByType(){
                 {field: 'sourceBranchName',title:'发货机构',width:'85px',align:'left'},
                 {field: 'inFormNo', title: '要货单号', width: '150px', align: 'left',
                     formatter:function(value,row,index){
+                        if (!value) {
+                            return '';
+                        }
                         var hrefStr='parent.addTab("详情","'+contextPath+'/form/deliverForm/deliverEdit?report=close&deliverFormId='+row.inFormId+'")';
                         return '<a style="text-decoration: underline;" href="#" onclick='+hrefStr+'>' + value + '</a>';
                     }
